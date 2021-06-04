374b07522ccf8de721bdd15d308cae01c532f080
Sam-McCathie <sammccathie2@gmail.com>
2021-06-04 13:56:16 +1200

    login working. No redirect currently

--- mission4/server/index.js
+++ mission4/server/index.js
@@ -33,7 +33,20 @@ app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  db.query("SELECT");
+  db.query(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send({err: err});
+      }
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({message: "Wrong username/ password combination!"});
+      }
+    }
+  );
 });
 
 //might need to change port?
--- mission4/src/Components/Re-use/Login.jsx
+++ mission4/src/Components/Re-use/Login.jsx
@@ -1,23 +1,51 @@
 import React, {useState} from "react";
 import "./Login.css";
+import Axios from "axios";
 
 function Login() {
-  const [usernameReg, setUsernameReg] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginStatus, setLoginStatus] = useState("");
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(response.data[0].username);
+      }
+    });
+  };
 
   return (
     <div className="Login">
       <h1>Login</h1>
       <div>
         <label>Username</label>
-        <input type="text" />
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
       </div>
       <div>
         <label>Password</label>
-        <input type="text" />
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </div>
       <div className="button">
-        <button>Login</button>
+        <button onClick={login}>Login</button>
       </div>
+      <p>{loginStatus}</p>
     </div>
   );
 }
