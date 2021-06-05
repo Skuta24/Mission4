cfe52a0de64dd484c580c10bd761947846cdd9c3
Sam-McCathie <sammccathie2@gmail.com>
2021-06-05 17:16:08 +1200

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {Route, BrowserRouter, Switch} from "react-router-dom";
+import {Route} from "react-router-dom";
 
 //Pages
 import Home from "./Components/Pages/Home/Home";
